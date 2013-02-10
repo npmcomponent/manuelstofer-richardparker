65417ba994c69d72ef110e1cdac9cdf3f3d61007
Manuel Stofer <manuelstofer@gmail.com>
2013-02-10 16:00:35 +0100

    Refactores index.js

--- index.js
+++ index.js
@@ -107,6 +107,98 @@ function getMacroNames (macros) {
 }
 
 /**
+ * Some bundled macros
+ */
+var nativeMacros = {
+
+    /**
+     * Resolves a value
+     * for example:
+     * {.pages[3].title} or also just {.}
+     *
+     * @param tree
+     * @return {String}
+     */
+    '.': function (tree) {
+        var path = helper.parsePath(tree);
+        return '__out.push(resolve(path + "' + path + '"));';
+    },
+
+    /**
+     * Moves down in path
+     * {-> .person.name {path}} -> .person.name
+     * @param tree
+     * @param transform
+     * @return {String}
+     */
+    '->': function (tree, transform) {
+        var path = helper.parsePath(tree);
+        return helper.keepPath(
+            'path += "' + helper.escapeJS(path) + '";\n' +
+                helper.transformTree(tree, transform) + '\n'
+        );
+    },
+
+    /**
+     * Checks if a path exists
+     * for example:
+     * {has .title <h1>{.title}</h1>}
+     *
+     * @param tree
+     * @param transform
+     * @return {String}
+     */
+    has: function (tree, transform) {
+        var path = helper.parsePath(tree);
+        return  'if (typeof resolve(path + "' + path + '") !== "undefined") {\n' +
+            helper.transformTree(tree, transform) + '\n' +
+            '}';
+    },
+
+    /**
+     * Iterates over an array or object
+     * for example:
+     * <ul>
+     *  {each .pages
+     *    <li>{.title}</li>
+     *  }
+     * </ul>
+     *
+     * @param tree
+     * @param transform
+     * @return {String}
+     */
+    each: function (tree, transform) {
+        var path = helper.parsePath(tree);
+        return helper.keepPath(
+            'path += "' + path + '";\n' +
+                'each(resolve("' + path + '"), function (__itemPath) {' +
+                helper.keepPath('path += "." + __itemPath; ' + helper.transformTree(tree, transform)) + '\n' +
+                '});'
+        );
+    },
+
+    /**
+     * Outputs the current path
+     * for example:
+     * {each .foo {path .bar}, } -> .foo[0].bar, .foo[1].bar ...
+     * @param tree
+     * @return {String}
+     */
+    path: function (tree) {
+        var path = helper.parsePath(tree);
+        return '__out.push(path + "' + path + '");';
+    },
+
+    /**
+     * Output ast, for internal use
+     */
+    out: function (tree, transform) {
+        return helper.transformTree(tree, transform);
+    }
+};
+
+/**
  * Some helper functions for generating javascript in macros
  *
  * @type {Object}
@@ -205,97 +297,6 @@ var helper = {
 };
 
 compile.helper = helper;
-
-/**
- * Some bundled macros
- */
-var nativeMacros = {
-
-    /**
-     * Resolves a value
-     * for example:
-     * {.pages[3].title} or also just {.}
-     *
-     * @param tree
-     * @return {String}
-     */
-    '.': function (tree) {
-        var path = helper.parsePath(tree);
-        return '__out.push(resolve(path + "' + path + '"));';
-    },
-
-    /**
-     * Moves down in path
-     * {-> .person.name {path}} -> .person.name
-     * @param tree
-     * @param transform
-     * @return {String}
-     */
-    '->': function (tree, transform) {
-        var path = helper.parsePath(tree);
-        return helper.keepPath(
-            'path += "' + helper.escapeJS(path) + '";\n' +
-            helper.transformTree(tree, transform) + '\n'
-        );
-    },
-
-    /**
-     * Checks if a path exists
-     * for example:
-     * {has .title <h1>{.title}</h1>}
-     *
-     * @param tree
-     * @param transform
-     * @return {String}
-     */
-    has: function (tree, transform) {
-        var path = helper.parsePath(tree);
-        return  'if (typeof resolve(path + "' + path + '") !== "undefined") {\n' +
-                    helper.transformTree(tree, transform) + '\n' +
-                '}';
-    },
-
-    /**
-     * Iterates over an array or object
-     * for example:
-     * <ul>
-     *  {each .pages
-     *    <li>{.title}</li>
-     *  }
-     * </ul>
-     *
-     * @param tree
-     * @param transform
-     * @return {String}
-     */
-    each: function (tree, transform) {
-        var path = helper.parsePath(tree);
-        return helper.keepPath(
-            'path += "' + path + '";\n' +
-            'each(resolve("' + path + '"), function (__itemPath) {' +
-                helper.keepPath('path += "." + __itemPath; ' + helper.transformTree(tree, transform)) + '\n' +
-            '});'
-        );
-    },
-
-    /**
-     * Outputs the current path
-     * for example:
-     * {each .foo {path .bar}, } -> .foo[0].bar, .foo[1].bar ...
-     * @param tree
-     * @return {String}
-     */
-    path: function (tree) {
-        var path = helper.parsePath(tree);
-        return '__out.push(path + "' + path + '");';
-    },
-
-    /**
-     * Output ast, for internal use
-     */
-    out: helper.transformTree
-};
-
 
 /**
  * Runtime method: resolving a path in the data
